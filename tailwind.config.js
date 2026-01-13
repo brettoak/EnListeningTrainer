c35693f447833f1616ff73a9bda95d4a7b685f54
Brett <12744007+brettoak@users.noreply.github.com>
2026-01-13 22:12:20 +1100

    update dark model

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
-    darkMode: 'class',
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {},
     },
